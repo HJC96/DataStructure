3bc3fc97c7708f42aa6a9c33bec364bf83d18a2c
Han Ji Chan <21500782@handong.edu>
2021-07-12 21:09:14 +0900

    Create Swap.c

---
+++ Swap.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+
+void Swap(int* px, int* py);
+
+int main(int argc, char* argv[]) {
+
+	int a = 100, b = 200;
+	printf("a = %d, b = %d\n", a, b);
+	Swap(&a, &b);
+	printf("a = %d, b = %d\n", a, b);
+
+
+}
+
+void Swap(int* px, int* py) {
+
+	int tmp;
+
+	tmp = *px;
+	*px = *py;
+	*py = tmp;
+
+}
