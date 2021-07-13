b84e5e1a9f1fee033902c446080d8ff4bc5970c2
Han Ji Chan <21500782@handong.edu>
2021-07-13 20:29:14 +0900

    Create malloc2.c

---
+++ malloc2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+
+	int *score;
+	int i;
+	
+	// 100 * sizeof(int) 바이트 만큼의 메모리를 동적으로 할당함.
+	score = (int*)malloc(100 * sizeof(int));
+	
+	if (score == NULL)
+	{
+		printf("동적 메모리 할당 오류\n");
+		exit(1);
+	}
+	for (i = 0; i < 100; i++)
+		score[i] = i;
+
+	free(score);
+
+	return 0;
+}
